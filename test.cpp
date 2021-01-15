716eb3871afbf4b530139645fd15efd938cb9ab7
Johny Tabrbouch <johnnyboomm@hotmail.com>
2021-01-15 21:15:09 +0100

    change test
    Adding new print command

--- test.cpp
+++ test.cpp
@@ -5,6 +5,12 @@ int main()
 {
     cout << "Hello World" << endl;
     cout << "Team Aufgabe 3" << endl;
+    cout << "Aenderung " << endl;
 
     return 0;
 }
+
+/*
+    Untracked files:
+    bedeutet: git wieß nich über die Dateien test und test.cpp, deswegen sollen wir das add command benutzen.
+*/
